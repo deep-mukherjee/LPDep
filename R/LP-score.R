9921a4edc159bba047600ce3737a0db118c1dbbc
Subhadeep(Deep) Mukhopadhyay <mesuvodeep@gmail.com>
2014-02-08 13:00:05 -0500

    LP-socre added

---
+++ R/LP-score.R
@@ -0,0 +1,25 @@
+#############################################################
+#
+# Construct LP-orthonormal score functions
+#
+# [Input X output TX]
+#
+##############################################################
+
+
+LP.score <- function(x,m){
+
+    n <- length(x)
+  
+    u <- (rank(x,"keep") - .5)/n #--mid-distribution transformation
+   
+   TS <- as.matrix(poly(u ,degree= min(length(unique(u ))-1, m)  ))  
+
+    for(j in 1:m){
+         TS[,j] <- TS[,j]/(sqrt( ((n-1)*var(TS[,j]))/n ))
+      }
+
+ return(TS)
+
+
+  }
