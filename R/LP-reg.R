ba4270078c449168e42cd5ae0e001db2cb7f58d9
Subhadeep(Deep) Mukhopadhyay <mesuvodeep@gmail.com>
2014-02-09 18:32:22 -0500

    LP-reg modified

--- R/LP-reg.R
+++ R/LP-reg.R
@@ -1,6 +1,5 @@
 #######################################
 #
-#
 #   LP Regression
 #
 #######################################
@@ -17,3 +16,30 @@ LP.reg <-function(y,X,m){
  return(tt)
 
    }
+
+
+#################################################
+#  Turlachs Example
+#################################################
+
+Turlach<-function(n,p){
+
+X <- matrix(runif(n*p),n,p)
+
+y <- (X[,1] - .5)^2 + apply(X[,2:5],1,sum) + rnorm(n,sd=.05)
+
+return(list(y=y,X=X))
+}
+
+
+T <- Turlach(n=500,p=50)
+y <-T$y
+X<-T$X
+
+Tx <- Score.mat(as.matrix(X),2)
+fit1 <- glmnet(Tx,y)
+plot(fit1,label=TRUE)
+
+
+
+
