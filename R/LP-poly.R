a0faea8e3f8ec2e76ed35bb4c14bf35bde8698be
Subhadeep(Deep) Mukhopadhyay <mesuvodeep@gmail.com>
2014-02-10 08:44:01 -0500

    small correction

--- R/LP-poly.R
+++ R/LP-poly.R
@@ -8,28 +8,26 @@
 
 
 LP.poly <- function(x,m){ #----for X random variable
-
-
     n <- length(x)
-  
     u <- (rank(x,"keep") - .5)/n #--mid-distribution transformation
-   
    TS <- as.matrix(poly(u ,degree= min(length(unique(u ))-1, m)  ))  
 
     for(j in 1:m){
          TS[,j] <- TS[,j]/(sqrt( ((n-1)*var(TS[,j]))/n ))
       }
- return(TS)
 
+ return(TS)
   }
 
 
 Score.mat <-function(X,m){  #----for X random vector
   X<- as.matrix(X)
   SX <- c()
-  for(j in 1:ncol(U)){
+
+  for(j in 1:ncol(X)){
       SX <- as.matrix( cbind(SX,LP.poly(X[,j],m) ) ) 
       }
+
     return(SX)
    }
 
